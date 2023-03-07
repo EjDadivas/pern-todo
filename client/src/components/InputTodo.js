04fa76bc7d407d2da4dabdaa52434a9d10c14ab7
Esther Joy Dadivas <99721216+EjDadivas@users.noreply.github.com>
2023-03-07 17:01:04 +0800

    input todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,16 +1,33 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 const InputTodo = () => {
+  const [description, setDescription] = useState("Hello");
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const body = description;
+      const response = fetch(
+
+      {
+        //"Content-Type" : "application/JSON"
+        //JSON.stringify
+      }
+      )
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
   return (
     <Fragment>
-    
-    <div>
-      <h1>Todo List</h1>
+      
+
+      <h1 className='text-center mt-5'>Todo List</h1>
       <form className='d-flex mt-5'>
-        <input className='form-control' type="text"></input>
+        <input className='form-control' type="text" value={description} onChange={e => setDescription(e.target.value)}></input>
         <button className='btn btn-success'>Add</button>
       </form>
-    </div>
+
     </Fragment >
   )
 }
